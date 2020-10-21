0014969ff55e8ca268db8e0114e1b9b0d60fffb5
blox-dev <bloxorz51gaming@gmail.com>
2020-10-21 22:53:47 +0300

    Fixed(?) animator

--- Assets/interactO.cs
+++ Assets/interactO.cs
@@ -5,48 +5,27 @@ using UnityEngine;
 
 public class interactO : MonoBehaviour
 {
-    public Animator animator;
-    public bool grab = false;
-    public bool metal = false;
-    public bool point = false;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        animator.SetBool("grab", false);
-        animator.SetBool("metal", false);
-        animator.SetBool("point", false);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown("q"))
         {
-            animator.SetBool("grab", true);
-            Task.Delay(1000).ContinueWith((task) => {
-                animator.SetBool("grab", false);
-            }); //nu prea merge
+            animator.SetTrigger("DefaultToGrab");
         }
         if (Input.GetKeyDown("w"))
         {
-            animator.SetBool("metal", true);
-            Task.Delay(1000).ContinueWith((task) => {
-                animator.SetBool("metal", false);
-            });
+            animator.SetTrigger("DefaultToPoint");
         }
         if (Input.GetKeyDown("e"))
         {
-            animator.SetBool("point", true);
-            Task.Delay(1000).ContinueWith((task) => {
-                animator.SetBool("point", false);
-            });
-        }
-        if (Input.GetKeyDown("r"))
-        {
-            animator.SetBool("grab", false);
-            animator.SetBool("metal", false);
-            animator.SetBool("point", false);
+            animator.SetTrigger("DefaultToMetal");
         }
     }
 }
